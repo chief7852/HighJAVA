2a5473ca778f4a0ff4ecbd8e0f10d1e834eacc54
ISSAC7852 <su951100@gmail.com>
2021-01-11 18:30:00 +0900

    2021-1-11 java Thread base

---
+++ workspace/enumTest/src/kr/or/ddit/basic/ConstTest.java
@@ -0,0 +1,14 @@
+package kr.or.ddit.basic;
+
+public class ConstTest {
+	public final static int RED = 1;
+	public final static int GREEN = 2;
+	public final static int BLUB = 3;
+	
+	public final static int ONE = 1;
+	public final static int TWO = 2;
+	public final static int THREE = 3;
+	
+	
+	
+}
