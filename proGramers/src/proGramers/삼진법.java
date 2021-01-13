06d23c36f565d9eace7dad208ff1fd76db421b22
ISSAC7852 <su951100@gmail.com>
2021-01-13 23:45:59 +0900

    no message

---
+++ proGramers/src/proGramers/삼진법.java
@@ -0,0 +1,24 @@
+package proGramers;
+
+public class 삼진법 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =5;
+		int b =24;
+		String[] day ={"fri","sat","sun","mon","tue","wed","thu"};
+		int allday = a*30 + b;
+		
+			System.out.println(day[allday%7]);
+		
+		
+
+	}
+}
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        return answer;
+    }
+}
+
