da6f2f4b56008df3f3730ffbcb9f79f811665fd1
ISSAC7852 <su951100@gmail.com>
2021-02-08 20:57:27 +0900

    no message

---
+++ workspace/Hankkeut/src/insertWord/InsertWord.java
@@ -0,0 +1,10 @@
+package insertWord;
+
+public class InsertWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ workspace/Hankkeut/src/kr/or/ddit/vo/WordVO.java
@@ -0,0 +1,6 @@
+package kr.or.ddit.vo;
+
+public class WordVO {
+	private String word;
+	private int w_no;
+}
